96a3514b25f6e8e47cf18614867eef649964d0b4
Justinas Grigas <jstn_as@protonmail.com>
2022-02-11 14:59:22 +0000

    implemented base movement script
    the script will be used by both the player and the ai

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Grid grid;
+    [SerializeField] private Floor floor;
+    protected Direction direction;
+    private Vector2Int _position;
+
+    protected virtual void Update()
+    {
+        Draw();
+    }
+
+    protected virtual void Move()
+    {
+        var offset = direction switch
+        {
+            Direction.Up => Vector2Int.up,
+            Direction.Down => Vector2Int.down,
+            Direction.Left => Vector2Int.left,
+            Direction.Right => Vector2Int.right,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        var newPosition = _position + offset;
+        if (!floor.IsValidTile(newPosition))
+            return;
+        _position = newPosition;
+    }
+
+    private void Draw()
+    {
+        var t = transform;
+        var cellSize = grid.cellSize;
+        var halfCell = cellSize / 2;
+        var targetPosition = new Vector3(_position.x * cellSize.x + halfCell.x, _position.y * cellSize.y + halfCell.y, 0);
+        t.position = Vector3.Lerp(t.position, targetPosition, speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,51 +1,26 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : Movement
 {
-    [SerializeField] private Grid grid;
-    [SerializeField] private Floor floor;
-    private Vector2Int _position;
-    private Vector2 _cellSize,
-        _halfCell;
-
-    private void Awake()
-    {
-        _cellSize = grid.cellSize;
-        _halfCell = _cellSize * 0.5f;
-    }
-
-    private void Update()
+    protected override void Update()
     {
         Move();
-        Draw();
+        base.Update();
     }
 
-    private void Move()
+    protected override void Move()
     {
         // Get input.
-        var input = Vector2Int.zero;
         if (Input.GetKeyDown(KeyCode.W))
-            input = Vector2Int.up;
+            direction = Direction.Up;
         else if (Input.GetKeyDown(KeyCode.A))
-            input = Vector2Int.left;
+            direction = Direction.Left;
         else if (Input.GetKeyDown(KeyCode.S))
-            input = Vector2Int.down;
+            direction = Direction.Down;
         else if (Input.GetKeyDown(KeyCode.D))
-            input = Vector2Int.right;
-        // Only move on input.
-        if (input == Vector2Int.zero)
+            direction = Direction.Right;
+        else
             return;
-        var newPosition = _position + input;
-        // Skip if can't move to new position.
-        if (!floor.IsValidTile(newPosition))
-            return;
-        _position = newPosition;
-    }
-
-    private void Draw()
-    {
-        var t = transform;
-        var targetPosition = new Vector3(_position.x * _cellSize.x + _halfCell.x, _position.y * _cellSize.y + _halfCell.y, 0);
-        t.position = Vector3.Lerp(t.position, targetPosition, 0.1f);
+        base.Move();
     }
 }
